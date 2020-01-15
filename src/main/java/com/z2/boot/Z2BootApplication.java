df2461fd43fbc0b26ed030ba58902462b017490d
zuoming.yi <zuoming.yi@dmall.com>
2020-01-15 10:42:22 +0800

    init

---
+++ src/main/java/com/z2/boot/Z2BootApplication.java
@@ -0,0 +1,13 @@
+package com.z2.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Z2BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Z2BootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/z2/boot/Z2BootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.z2.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Z2BootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
